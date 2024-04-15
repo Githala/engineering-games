3610a0c5c9fd6283236dcc08d83091328301d2f7
Arne de Both <arne@cloudwise.nl>
2024-04-15 22:18:25 +0200

    Initial commit of wave-game. Able to generate waves composed of one or more pure waves

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import Display from '@/components/Display.vue';
+import {ref} from 'vue';
+import RotaryEncoder from '@/components/RotaryEncoder.vue';
+
+const waveFunc = ref([
+  { a: 1, b: 1, c: 2 },
+  { a: 2, b: 3, c: 4 },
+  { a: 1, b: 10, c: 3 }
+])
+
+function addA(a) {
+  waveFunc.value.a+=a;
+}
+function addB(b) {
+  waveFunc.value.b += b;
+}
+function addC(c) {
+  waveFunc.value.c += c;
+}
+
+
+</script>
+
+<template>
+  <main>
+    <Display v-model="waveFunc" />
+  </main>
+  <div>
+    <RotaryEncoder></RotaryEncoder>
+    <div>
+      <button @click="addA(1)">A++</button>
+      <button @click="addA(-1)">A--</button>
+    </div>
+    <div>
+      <button @click="addB(1)">B++</button>
+      <button @click="addB(-1)">B--</button>
+    </div>
+    <div>
+      <button @click="addC(1)">C++</button>
+      <button @click="addC(-1)">C--</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/RotaryEncoder.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="circle">
+  <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <defs>
+      <linearGradient gradientUnits="userSpaceOnUse" x1="189.251" y1="179.572" x2="189.251" y2="244.234" id="gradient-0">
+        <stop offset="0" style="stop-color: rgb(25.882% 25.882% 25.882%)"></stop>
+        <stop offset="1" style="stop-color: rgb(16.241% 16.241% 16.241%)"></stop>
+      </linearGradient>
+    </defs>
+    <ellipse style="fill: rgb(66, 66, 66); paint-order: fill; stroke: rgb(152, 152, 152); stroke-width: 2px;" cx="50" cy="50.071" rx="46" ry="46"></ellipse>
+    <rect x="48.6" width="3" height="21.417" style="stroke: rgb(255, 255, 255); fill: rgb(255, 255, 255);" y="9"></rect>
+  </svg>
+  </div>
+</template>
+
+<style scoped>
+  .circle {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+    //rotate: 15deg;
+  }
+</style>
